8018888eea9ff1608aeafd48f871b8fc3e29b7b5
Naru Tejeswar Reddy <98583260+TejeswarReddyNaru@users.noreply.github.com>
2022-02-11 14:37:34 +0530

    Update 05_clap_number.py

--- 05_clap_number.py
+++ 05_clap_number.py
@@ -23,7 +23,13 @@ Explanation:
 # Implement the below function and run the program
 
 def clap_number(n):
-    pass
+  list1=[]
+  for i in range(1,n+1):
+    if "3" in str(i) or "6" in str(i) or "9" in str(i) or i%3==0:
+      list1.append("clap")
+    else:
+      list1.append(str(i))
+  return list1
 
 
 class TestClapNumber(unittest.TestCase):
